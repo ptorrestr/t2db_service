0c1ad8b3a5ce087d550e7f9bb060366d8592479b
Pablo Torres <pablo.torres.t@gmail.com>
2013-06-11 15:56:16 +0100

    Adding prefix for requests and responses

--- searchrests/urls.py
+++ searchrests/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('searchrests.views',
     url(r'^searchrests/$', 'searchrest_list'),
     url(r'^searchrests/(?P<pk>[0-9]+)/$', 'searchrest_detail'),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
