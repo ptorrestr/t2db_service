c1a658ac19458edc7f4693cb536d9b2ffbc8f301
Pablo Torres <pablo.torres.t@gmail.com>
2013-06-12 12:00:11 +0100

    RESTful service ready

--- searchrests/urls.py
+++ searchrests/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
+from searchrests import views
 
-urlpatterns = patterns('searchrests.views',
-    url(r'^searchrests/$', 'searchrest_list'),
-    url(r'^searchrests/(?P<pk>[0-9]+)/$', 'searchrest_detail'),
+urlpatterns = patterns('',
+    url(r'^searchrests$', views.SearchrestList.as_view()),
+    url(r'^searchrests/new$', views.SearchrestNew.as_view()),
+#    url(r'^searchrests/$', views.SearchrestList.as_view()),
+    url(r'^searchrests/(?P<pk>[0-9]+)$', views.SearchrestDetail.as_view()),
+#    url(r'^searchrests/(?P<pk>[0-9]+)/$', views.SearchrestDetail.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- searchrests/views.py
+++ searchrests/views.py
@@ -1,49 +1,21 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from searchrests.models import Searchrest
 from searchrests.serializers import SearchrestSerializer
+from rest_framework import generics
+from rest_framework.response import Response
 
-@api_view(['GET', 'POST'])
-def searchrest_list(request, format = None):
-    """
-    List all searchrests, or create a new searchrest.
-    """
-    if request.method == 'GET':
-        searchrests = Searchrest.objects.all()
-        serializer = SearchrestSerializer(searchrests, many=True)
+class SearchrestList(generics.ListCreateAPIView):
+    queryset = Searchrest.objects.all()
+    serializer_class = SearchrestSerializer
+
+class SearchrestNew(generics.RetrieveAPIView):
+    queryset = Searchrest.objects.all()
+    serializer_class = SearchrestSerializer
+
+    def get(self, request, *args, **kwargs):
+        searchrest_first = Searchrest()
+        serializer = SearchrestSerializer(searchrest_first)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = SearchrestSerializer(data=request.DATA)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def searchrest_detail(request, pk, format = None):
-    """
-    Retrieve, update or delete a searchrest instance.
-    """              
-    try:
-        searchrest = Searchrest.objects.get(pk=pk)
-    except Searchrest.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = SearchrestSerializer(searchrest)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = SearchrestSerializer(searchrest, data=request.DATA)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        searchrest.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class SearchrestDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Searchrest.objects.all()
+    serializer_class = SearchrestSerializer
