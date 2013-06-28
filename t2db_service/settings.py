b1d88bd7a21f34c25871c2461bc19989e44fc301
Pablo Torres <pablo.torres.t@gmail.com>
2013-06-28 11:40:42 +0100

    add mysql support

--- t2db_service/settings.py
+++ t2db_service/settings.py
@@ -12,10 +12,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'service.db',                    
+        'NAME': 'service.db',
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'quiltro_api_production',                    
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'quiltro',
+        'PASSWORD': 'quiltroadmin',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
